9600c756a1680b0808802d4d5c3018d8e2c4cd86
Neill Landman <neill@avochoc.com>
2018-10-15 20:34:22 +0200

    -

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Landman\MultiTokenAuth\Auth\TokensGuard;
 use Landman\MultiTokenAuth\Classes\TokenApp;
 use Landman\MultiTokenAuth\Console\Commands\MakeApiClient;
+use Landman\MultiTokenAuth\Events\ApiAuthenticated;
+use Landman\MultiTokenAuth\Events\ApiAuthenticating;
+use Landman\MultiTokenAuth\Events\ApiLogin;
+use Landman\MultiTokenAuth\Events\ApiLogout;
+use Landman\MultiTokenAuth\Events\ApiRegistered;
 use Landman\MultiTokenAuth\Models\ApiClient;
 use Landman\MultiTokenAuth\Models\ApiToken;
 use Tests\TestCase;
@@ -18,6 +23,8 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     use DatabaseTransactions;
+
+    protected $auth = null;
 
     /**
      * A basic test example.
@@ -43,7 +50,8 @@ class ExampleTest extends TestCase
         $this->assertInstanceOf(TokensGuard::class, TokenApp::guard());
     }
 
-    public function testMakeClient(){
+    public function testMakeClient()
+    {
         $name = str_random(16);
         $client = ApiClient::make($name);
         $this->assertDatabaseHas(TokenApp::config('table_clients'), [
@@ -75,7 +83,7 @@ class ExampleTest extends TestCase
     /**
      *
      */
-    public function testLoginAndUserRoutes()
+    public function testLogiRoutes()
     {
         $passwordField = TokenApp::config('password_field');
         $usernameField = TokenApp::config('username');
@@ -83,14 +91,14 @@ class ExampleTest extends TestCase
             $passwordField => bcrypt('secret'),
         ]);
 
-        $client = ApiClient::make(str_random(16));
 
         $requestData = [
-            'client_id' => $client->value,
+            'client_id' => ApiClient::make(str_random(16))->value,
             $usernameField => $user->{$usernameField},
             $passwordField => 'secret',
         ];
 
+        $this->expectsEvents([ApiLogin::class]);
         $response = $this->json('POST', TokenApp::routeUri('login'), $requestData);
 
         $response->assertSuccessful();
@@ -103,13 +111,110 @@ class ExampleTest extends TestCase
         $this->assertDatabaseHas(TokenApp::config('table_tokens'), [
             'token' => $responseData['token'],
         ]);
+    }
 
-        $response = $this->json('GET', TokenApp::routeUri('user'), $requestData);
+    public function testRegisterRoute()
+    {
+        $client = ApiClient::make(str_random());
+        $this->assertDatabaseHas(TokenApp::config('table_clients'), [
+            'id' => $client->id,
+        ]);
+        $user = factory(TokenApp::getUserClass())->make([
+            'password' => 'secret_password',
+            'password_confirmation' => 'secret_password',
+            'device' => 'SecretTestDevice',
+            'client_id' => $client->value,
+        ]);
+        $user->makeVisible(['password']);
+        $data = $user->toArray();
 
-        $response->assertSuccessful();
+        $response = $this->json('POST', TokenApp::routeUri('register'), $data);
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'user' => [
+                TokenApp::config('username'),
+            ],
+            'token',
+        ]);
+
+        $this->assertDatabaseHas(TokenApp::makeUserModel()->getTable(), array_only($data, [
+            TokenApp::config('username'),
+        ]));
+
+//        $this->json()
+    }
+
+    public function testUserRoute()
+    {
+        // Assert via Authorization header
+        $this->expectsEvents([ApiAuthenticating::class, ApiAuthenticated::class]);
+
+        $response = $this->json('GET', TokenApp::routeUri('user'), [], $this->auth()->headers);
+
+        $response->assertStatus(200);
 
         $responseData = json_decode($response->getContent(), true);
+        $usernameField = TokenApp::config('username');
         $response->assertJsonStructure([$usernameField]);
-        $this->assertEquals($responseData[$usernameField], $user->{$usernameField});
+        $this->assertEquals($responseData[$usernameField], $this->auth()->user->{$usernameField});
+    }
+
+    public function testLogout()
+    {
+        $user = $this->auth()->user;
+
+        $this->expectsEvents([ApiLogout::class]);
+        $response = $this->json('POST', TokenApp::routeUri('logout'), [], $this->auth()->headers);
+        $response->assertStatus(200);
+        $token = $this->auth()->token;
+
+        $this->assertSoftDeleted(TokenApp::config('table_tokens'), [
+            'id' => $token->id, 'token' => $token->token,
+        ]);
+        $response->assertJson(['success' => 1]);
+    }
+
+
+    public function testLogoutAll()
+    {
+        $user = $this->auth()->user;
+        // Assign 9 more tokens to user.
+        for ($i = 0; $i < 9; $i++) {
+            $user->issueToken();
+        }
+
+        $this->assertGreaterThan(1, $user->apiTokens()->count());
+
+        $this->expectsEvents([ApiLogout::class]);
+        $response = $this->json('POST', TokenApp::routeUri('logout_all'), [], $this->auth()->headers);
+        $response->assertStatus(200);
+        $response->assertJson(['success' => 1]);
+
+        $this->assertEquals(0, $user->apiTokens()->count());
+    }
+
+    /**
+     * @return \stdClass
+     */
+    protected function auth()
+    {
+        if ($this->auth)
+            return $this->auth;
+
+        $user = factory(TokenApp::getUserClass())->create();
+        $token = $user->issueToken();
+        $this->assertDatabaseHas(TokenApp::config('table_tokens'), [
+            'id' => $token->id, 'token' => $token->token
+        ]);
+        $auth = [
+            'user' => $user,
+            'token' => $token,
+            'headers' => [
+                'Authorization' => "Bearer {$token->token}"
+            ]
+        ];
+        $this->auth = (object)$auth;
+        return $this->auth;
     }
 }
